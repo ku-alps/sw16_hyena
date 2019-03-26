ce25e0c2f881f9c892ffeaae6516d3dfad629423
Hyenakim <gpsk1202@gmail.com>
2019-03-27 03:05:57 +0900

    로프

---
+++ Default/로프.java
@@ -0,0 +1,28 @@
+package Default;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class ทฮวม {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++) 
+			arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		int max = Integer.MIN_VALUE;
+		for(int i=0;i<n;i++) 
+			max = Math.max(max, arr[i]*(n-i));	
+		bw.write(String.valueOf(max));
+		bw.close();
+	}
+
+}
